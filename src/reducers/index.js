5456545b418550b8441a26286502d76fff1c9520
Michelle D Zhang <zhang.michelle.d@gmail.com>
2017-02-20 11:40:41 -0500

    Add header bar with title (#2)
    Use Material UI as ui component library

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 
+import termReducer, { STATE_KEY as TERM_STATE_KEY } from 'src/reducers/terms';
+
 const rootReducer = combineReducers({
+  [TERM_STATE_KEY]: termReducer,
 });
 
 export default rootReducer;
--- src/views/AppView.jsx
+++ src/views/AppView.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from 'src/components/Header';
 
 const AppView = () => {
   return (
     <div>
+      <Header />
       Hello world!
     </div>
   );
